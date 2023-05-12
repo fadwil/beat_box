a8b178b21a1eb799356e1838f175adc651a97805
Wil Fady <wrfady@gmail.com>
2023-05-11 22:36:12 -0400

    test node

---
+++ spec/node_spec.rb
@@ -0,0 +1,15 @@
+require './lib/node'
+
+RSpec.describe Node do
+  it 'is an instance of node' do
+    node = Node.new("plop")
+
+    expect(node).to be_instance_of(Node)
+  end
+
+  it 'has a next node' do
+    node = Node.new("plop")
+
+    expect(node.next_node).to eq nil
+  end
+end
