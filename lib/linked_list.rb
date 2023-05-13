2b54e8769cd907a5452745fa2b6fc87e369dd059
Wil Fady <wrfady@gmail.com>
2023-05-13 12:09:39 -0400

    Add counter

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,16 +1,20 @@
 class LinkedList
+  attr_reader :count
   attr_accessor :head
   def initialize
     @head = nil
+    @count = 0
   end
 
   def append(data)
     if @head.nil?
-      @head = Node.new(data)
+      @head = Node.new(data)  
     else
       data.next_node = @head
       @head = data
     end
+    @count += 1
     data
   end
+
 end
