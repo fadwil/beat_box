7ea93ab4a349b8bc99cd88357efe40801886b8dd
Wil Fady <wrfady@gmail.com>
2023-05-13 16:27:01 -0400

    Add to_string

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,19 +1,51 @@
 class LinkedList
-  attr_reader :count
   attr_accessor :head
   def initialize
     @head = nil
-    @count = 0
   end
 
   def append(data)
     if @head.nil?
       @head = Node.new(data)  
     else
-      @head.next_node = Node.new(data)
+      current_node = @head
+      until current_node.next_node.nil?
+        current_node = current_node.next_node
+      end
+      current_node.next_node = Node.new(data)
     end
-    @count += 1
     data
   end
+    
+  def count
+    current_node = @head
+    count = 0
+    if @head.nil?
+      count
+    else
+      until current_node.next_node.nil?
+        count += 1
+        current_node = current_node.next_node
+      end
+      count += 1
+    end
+  end
+  
+  def to_string
+    current_node = @head
+    string = ""
+    if head.nil?
+      string
+    else
+      until current_node.next_node.nil?
+        string << "#{current_node.data} "
+        current_node = current_node.next_node
+      end
+    string << "#{current_node.data}"
+    end
+  end
 
+
+
+# need a current node as a placeholder to traverse the linked list
 end
