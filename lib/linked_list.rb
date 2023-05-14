10853c2d46b4bf6823c4e3d1a37f5406de3ff17e
Wil Fady <wrfady@gmail.com>
2023-05-14 18:58:19 -0400

    Add insert method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -16,7 +16,37 @@ class LinkedList
     end
     data
   end
-    
+  
+  def prepend(data)
+    node = Node.new(data)
+    current_node = @head
+   if @head.nil?
+    @head = Node.new(data)
+   else
+    node.next_node = current_node
+    @head = node
+    end
+    data
+  end
+   
+  def insert(position, data)
+    if position == 0 
+      prepend(data)
+    else
+      node = Node.new(data)
+      current_node = @head
+      (position - 1).times do 
+        current_node = current_node.next_node
+      end
+      node.next_node = current_node.next_node
+      current_node.next_node = node
+      node.data
+    end
+  end
+  # insert will insert one or more elements at a given position in the list. It takes two parameters, the first one is the position at which to insert nodes, the second parameter is the string of data to be inserted.
+
+  # insert needs the ability to iterate through the linked list, like iterating through an array. It needs to be able to find the position specified (the first of the two arguments) and then it needs to be able to place the data (the second of the two arguments) in that position specified. As nodes point to the next node, it will need the previous node to point to it, and it will need to point to the next node. It also needs to return the data passed in the argument.
+
   def count
     current_node = @head
     count = 0
@@ -44,8 +74,7 @@ class LinkedList
     string << "#{current_node.data}"
     end
   end
-
-
+end
 
 # need a current node as a placeholder to traverse the linked list
-end
+
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -52,10 +52,23 @@ RSpec.describe LinkedList do
     list = LinkedList.new
     list.append("plop")
     list.append("suu")
-    list.prepend("dop")
-    expect(list.prepend).to eq("dop")
+    
+    expect(list.prepend("dop")).to eq("dop")
     expect(list.to_string).to eq("dop plop suu")
   end
+
+  it 'has insert method' do
+    list = LinkedList.new
+    list.append("plop")
+    expect(list.to_string).to eq("plop")
+    list.append("suu")
+    list.prepend("dop")
+    expect(list.to_string).to eq("dop plop suu")
+    expect(list.count).to eq(3)
+    expect(list.insert(1, "woo")).to eq("woo")
+    expect(list.to_string).to eq("dop woo plop suu") 
+  end
+  
 end
 
     
