1912d27ff256f5c1f9875e59b84ba3b5c32074b7
Wil Fady <wrfady@gmail.com>
2023-05-16 18:28:54 -0400

    Add count and play methods, refactor append method

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,6 +1,24 @@
 class BeatBox
-  attr_reader :list
+  attr_reader :list, :data
   def initialize
     @list = LinkedList.new
+    @data = data
+  end
+
+  def append(data)
+    words = data.split
+    words.each do |word|
+    @list.append(word)
+    end
+  end
+
+  def count
+    @list.count
+  end
+
+  def play
+    beats = @list.to_string
+    `say -r 500 -v Alex #{beats}`
+    beats
   end
 end
