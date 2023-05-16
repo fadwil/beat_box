2f9e04b8c77c9e041f205fc4a53bc6515cfa4947
Wil Fady <wrfady@gmail.com>
2023-05-16 18:29:24 -0400

    Add tests for count and play

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -15,5 +15,35 @@ RSpec.describe BeatBox do
     expect(bb.list).to be_instance_of(LinkedList)
   end
 
-  
+  it 'has a head' do
+    bb = BeatBox.new
+
+    expect(bb.list.head).to eq nil
+  end
+
+  it 'has append method' do
+    bb = BeatBox.new
+    expect(bb.list.head).to eq nil
+    bb.append("deep doo ditt")
+    expect(bb.list.append("deep doo ditt")).to eq("deep doo ditt")
+  end
+
+  it 'can access head data' do
+    bb = BeatBox.new
+    bb.append("deep doo ditt")
+    expect(bb.list.head.data).to eq("deep")
+  end
+
+  it 'has a count method' do
+    bb = BeatBox.new
+    bb.append("deep doo ditt")
+    bb.append("woo hoo shu")
+    expect(bb.count).to eq(6)
+  end
+
+  it 'has a play method' do
+    bb = BeatBox.new
+    bb.append("woo hoo shu woo hoo shu")
+    bb.play
+  end
 end
