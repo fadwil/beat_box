731c6fdb66c59d3af259e1e84f2a6cc023318c21
Wil Fady <wrfady@gmail.com>
2023-05-15 23:59:54 -0400

    Initial commit

---
+++ spec/beat_box_spec.rb
@@ -0,0 +1,11 @@
+require "./lib/beat_box"
+require "./lib/linked_list"
+require "./lib/node"
+
+RSpec.describe BeatBox do
+  it 'is an instance of beat box' do
+    bb = BeatBox.new
+
+    expect(bb).to be_instance_of(BeatBox)
+  end
+end
